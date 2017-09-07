32ac12ec0d5a96248ead758c7e0ab9cfdb4ad1ce
Paul Young <yyang173@illinois.edu>
2017-09-06 20:15:19 -0500

    working version

---
+++ refresh.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# refresh executable on Titan
+# toolchain file should have everything
+#source ../env-for-qmcpack.sh
+
+if [ ! -d build ]; then
+  mkdir build
+  cd build
+  cmake -DCMAKE_TOOLCHAIN_FILE=../config/TitanGNU.cmake ..
+  sed -i 's/\/sw\/xk6\/subversion\/1.8.3\/sles11.1_gnu4.3.4\/bin\/svn//' CMakeCache.txt
+  cmake -DCMAKE_TOOLCHAIN_FILE=../config/TitanGNU.cmake .. # run a second time to pass in C flags
+else
+  cd build
+fi
+
+make qmcapp -j 16 
+
+cd ..
